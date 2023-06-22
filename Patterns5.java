265aa208528e65c64e29aa8fd8ddf6a492137559
Keertana_Ganiga <43719847+keertanaganiga@users.noreply.github.com>
2023-06-22 12:33:53 +0530

    Add files via upload

---
+++ Patterns5.java
@@ -0,0 +1,15 @@
+package java_course_udemy;
+
+public class Patterns5 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(i+j>5)
+					System.out.print("* ");
+				else
+					System.out.print("  ");
+			}
+			System.out.println();
+		}
+	}
+}
